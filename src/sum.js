11905c0fc1760df531a3939081adff84680877b3
uoon <uoonleen@gmail.com>
2018-02-15 17:48:51 +0900

    CommonJS module : how to link each other file

---
+++ src/sum.js
@@ -0,0 +1,8 @@
+const sum = (a, b) => a + b;
+
+/*
+  CommonJS Module
+  1. make a function, string, array, object and any value avaliable to other modules inside of my project
+*/
+
+module.exports = sum;
